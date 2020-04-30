7fcc10e60886479df698818ab8dc9aa704a5fdc0
maria-carolina <mariacarolina1701@outlook.com>
2020-04-30 13:13:26 -0300

    API de CEP e blade de erros

---
+++ resources/views/layouts/cep-api.blade.php
@@ -0,0 +1,84 @@
+<!-- Adicionando JQuery -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
+
+<!-- Adicionando Javascript -->
+<script type="text/javascript" >
+
+    //Script CEP
+    $(document).ready(function() {
+        'use strict';
+        function limpa_formulario_cep() {
+// Limpa valores do formulário de cep.
+            $("#rua").val("");
+            $("#bairro").val("");
+            $("#cidade").val("");
+            $("#uf").val("");
+        }
+
+//Quando o campo cep perde o foco.
+        $("#cep").blur(function () {
+//Nova variável "cep" somente com dígitos.
+            var cep = $(this).val().replace(/\D/g, '');
+
+//Verifica se campo cep possui valor informado.
+            if (cep != "") {
+
+//Expressão regular para validar o CEP.
+                var validacep = /^[0-9]{8}$/;
+
+//Valida o formato do CEP.
+                if (validacep.test(cep)) {
+
+//Preenche os campos com "..." enquanto consulta webservice.
+                    $("#rua").val("...");
+                    $("#bairro").val("...");
+                    $("#cidade").val("...");
+                    $("#uf").val("...");
+
+                    //Consulta o webservice viacep.com.br/
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+
+                        if (!("erro" in dados)) {
+//Atualiza os campos com os valores da consulta.
+                            $("#rua").prop('readonly', true);
+                            $("#bairro").prop('readonly', true);
+                            $("#cidade").prop('readonly', true);
+                            $("#uf").prop('readonly', true);
+
+                            $("#rua").val(dados.logradouro);
+                            $("#bairro").val(dados.bairro);
+                            $("#cidade").val(dados.localidade);
+                            $("#uf").val(dados.uf);
+
+                            if(dados.logradouro == ""){
+                                alert("Por favor preencha o formulário");
+                                $("#rua").prop('readonly', false);
+                                $("#bairro").prop('readonly', false);
+                                $("#cidade").prop('readonly', false);
+                                $("#uf").prop('readonly', false);
+                            }
+                        }
+                        else {
+//CEP pesquisado não foi encontrado.
+                            limpa_formulario_cep();
+                            alert("CEP não encontrado.");
+                        }
+                    });
+                }
+                else {
+//cep é inválido.
+                    limpa_formulario_cep();
+                    alert("Formato de CEP inválido.");
+                }
+            }
+            else {
+//cep sem valor, limpa formulário.
+                limpa_formulario_cep();
+            }
+        });
+    });
+
+
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,51 @@
 
 @section('container')
 
-   // o conteudo
+{{--    <!-- Inicio do formulario -->--}}
+{{--    <form method="" action="">--}}
+
+{{--        <div class="row align-items-end">--}}
+{{--            <div class="col-6">--}}
+{{--                <label for="cep">CEP:</label>--}}
+{{--                <input type="text" class="form-control" onkeypress="mask(this, '#####-###')" maxlength="9" id="cep" name="cep" placeholder="Digite o CEP (ex: 00000-000)" required>--}}
+{{--            </div>--}}
+{{--            <div class="col-3">--}}
+{{--                <button class="btn btn-primary">Carregar</button>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="row">--}}
+{{--            <div class="col-6">--}}
+{{--                <label for="rua">Rua:</label>--}}
+{{--                <input type="text" class="form-control" name="endereco" id="rua" required readonly>--}}
+{{--            </div>--}}
+{{--            <div class="col-3">--}}
+{{--                <label for="numero">Número:</label>--}}
+{{--                <input type="text" class="form-control" name="numero" id="numero" value="" required>--}}
+{{--            </div>--}}
+{{--            <div class="col-3">--}}
+{{--                <label for="complemento">Complemento:</label>--}}
+{{--                <input type="text" class="form-control" name="complemento" id="complemento">--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="row">--}}
+{{--            <div class="col-4">--}}
+{{--                <label for="cidade">Cidade:</label>--}}
+{{--                <input type="text" class="form-control" name="cidade" id="cidade" required readonly>--}}
+{{--            </div>--}}
+{{--            <div class="col-4">--}}
+{{--                <label for="Bairro">Bairro:</label>--}}
+{{--                <input type="text" class="form-control" name="bairro" id="bairro" required readonly>--}}
+{{--            </div>--}}
+{{--            <div class="col-4">--}}
+{{--                <label for="Uf">Estado:</label>--}}
+{{--                <input type="text" class="form-control" name="uf" id="uf" required readonly>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </form>--}}
 
 @endsection
+
+
+{{--@section('scripts_adicionais')--}}
+{{--    @includeIf('layouts.cep-api')--}}
+{{--@endsection--}}
